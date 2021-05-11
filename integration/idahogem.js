56dd8f8c11c62f8869f0d50be00afb2a82cd95c4
Alex Clark <amsclark@gmail.com>
2021-05-11 12:46:39 -0500

    Update idahogem.js

--- integration/idahogem.js
+++ integration/idahogem.js
@@ -3,7 +3,7 @@ drupalpath = window.location.href.split('/').slice(3,5).join('/')
 activepaths = ["topics/2477", "topics/2478", "topics/2782", "node/2265", "topics/2479", "node/2174", "node/1351"]
 
 // if current page is active, set up the dialogflow messenger
-if (activepaths.includes(drupalpath) || true) {
+if (activepaths.includes(drupalpath)) {
 
   info='<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>'
   document.write(info)
