70f5fddcd1cba3b4c9971104c1e54f59b3dc2f55
Alex Clark <amsclark@gmail.com>
2021-05-11 12:11:58 -0500

    Create idahogem.js

---
+++ integration/idahogem.js
@@ -0,0 +1,3 @@
+info='<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script><df-messenger intent="WELCOME" chat-title="Housing Reasonable Accomodation Chatbot" agent-id="e9b92b63-a89f-4458-bb09-04bf7ba6c663" language-code="en"></df-messenger>'
+
+document.write(info)
